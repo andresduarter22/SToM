8d6981b8beb7e46f1202ef96ac0a95abe86565e6
Andres <andresduarter13@gmail.com>
2018-05-23 18:43:01 -0400

    Clase ClienteWebApp

---
+++ src/main/java/upb/webapp/ClienteWebApp.java
@@ -0,0 +1,12 @@
+package upb.webapp;
+
+
+import javax.ws.rs.DELETE;
+import javax.ws.rs.Path;
+
+@Path("/cliente")
+public class ClienteWebApp {
+
+    //@DELETE
+   // @Path("/cliente/borrar/{id_cliente}")
+}
