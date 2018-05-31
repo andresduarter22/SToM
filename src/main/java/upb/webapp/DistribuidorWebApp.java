e9e6574bcf9c4ecc80306ad19bd9a5f55d776439
Andres <andresduarter13@gmail.com>
2018-05-30 22:59:14 -0400

    Delete, aun incompleto

--- src/main/java/upb/webapp/DistribuidorWebApp.java
+++ src/main/java/upb/webapp/DistribuidorWebApp.java
@@ -1,5 +1,32 @@
 package upb.webapp;
 
+import javax.ws.rs.DELETE;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+
+@Path("/distribuidor")
 public class DistribuidorWebApp {
+    private static DatabaseStom db = new DatabaseStom();
+
+
+
+
+
+    @DELETE
+    @Path("/borrar/{id_distribuidor}")
+    public javax.ws.rs.core.Response borrarCliente(@PathParam("id_distribuidor") int id_distribuidor) {
+
+        db.delete(id_distribuidor);
+        return  javax.ws.rs.core.Response
+                .status(200)
+                .header("Access-Control-Allow-Origin", "*")
+                .header("Access-Control-Allow-Headers", "origin, content-type, accept, authorization")
+                .header("Access-Control-Allow-Credentials", "true")
+                .header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD")
+                .header("Access-Control-Max-Age", "1209600")
+                .build();
+
+    }
+
 
 }
