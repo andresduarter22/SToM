3c89fcbd7082fb12ed5e3993bc8480bb9f29a3c9
Luis Fer <pastillalc@gmail.com>
2018-06-15 16:50:33 -0400

    principio devolucion juego

--- src/main/java/upb/webapp/DatabaseJuego.java
+++ src/main/java/upb/webapp/DatabaseJuego.java
@@ -1,8 +1,8 @@
 package upb.webapp;
 
 import com.sun.jersey.spi.resource.Singleton;
-import upb.entity.Juegos;
 import upb.entity.Compra;
+import upb.entity.Juegos;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -20,36 +20,7 @@ public class DatabaseJuego {
         ENTITY_MANAGER_FACTORY.close();
     }
 
-    public static Juegos createJuego(String nombre,
-                              String estado, String categoria, int costo, String version,int distribuidor, String descripcion, String linkImagen) {
-        // Create an EntityManager
-        EntityManager manager = ENTITY_MANAGER_FACTORY.createEntityManager();
-        EntityTransaction transaction = null;
-        Juegos juego= new Juegos(nombre, estado, categoria,costo,version, distribuidor,descripcion,linkImagen);
-        try {
-            // empieza transaccon
-            transaction = manager.getTransaction();
-            transaction.begin();
-            // crea objeto
-
-            // guarda libro persistentemente
-            manager.persist(juego);
-            // envia transaccion
-            transaction.commit();
-
-        } catch (Exception ex) {
-            if (transaction != null) {
-                transaction.rollback();
-            }
-            ex.printStackTrace();
-
-        } finally {
-            manager.close();
-        }
-        return juego;
-    }
-    public static Compra crearCompra(int id_juego,int id_cliente, int precio){
-
+    public static Compra crearCompra(int id_juego, int id_cliente, int precio){
         EntityManager manager = ENTITY_MANAGER_FACTORY.createEntityManager();
         EntityTransaction transaction = null;
         Compra compra= new Compra(id_juego,id_cliente,precio);
@@ -76,11 +47,44 @@ public class DatabaseJuego {
         return compra;
     }
 
-    public static void main(String[] args){
-    //    createJuego("kaiba adventures 2","released","hentai",800, "1.23a45r", 1,
-    //            "Est es la historia del grandiosisimo kaiba, maestro del hentai que busca conquistar a la princesa valeria.",
-    //           "http://i0.kym-cdn.com/entries/icons/original/000/025/897/kaiba.jpg");
-        crearCompra(4,1,50);
-        ENTITY_MANAGER_FACTORY.close();
+    public static void main(String[] args0) {
+        //    createJuego("kaiba adventures 2","released","hentai",800, "1.23a45r", 1,
+        //            "Est es la historia del grandiosisimo kaiba, maestro del hentai que busca conquistar a la princesa valeria.",
+        //           "http://i0.kym-cdn.com/entries/icons/original/000/025/897/kaiba.jpg");
+        crearCompra(4, 1, 50);
     }
+    public static String devolverJuego(int id){
+        System.out.println("devlover comprar: " + id);
+        EntityManager manager = ENTITY_MANAGER_FACTORY.createEntityManager();
+        EntityTransaction transaction = null;
+        String estado= "fallido";
+        boolean flag = true;
+        try {
+            // empieza transaccon
+            transaction = manager.getTransaction();
+            transaction.begin();
+            // crea objeto
+
+            // guarda libro persistentemente
+            Compra compraABorrar= manager.find(Compra.class,id);
+            // envia transaccion
+
+            if (compraABorrar != null) {
+                manager.remove(compraABorrar);
+                transaction.commit();
+                estado="exito";
+            }
+
+        } catch (Exception ex) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            ex.printStackTrace();
+
+        } finally {
+            manager.close();
+        }
+        return estado;
+    }
+
 }
--- src/main/java/upb/webapp/JuegosWebApp.java
+++ src/main/java/upb/webapp/JuegosWebApp.java
@@ -11,18 +11,8 @@ import javax.ws.rs.core.MediaType;
 public class JuegosWebApp {
 
     private static DatabaseJuego db = new DatabaseJuego();
-    @POST
-    @Path("/post")
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
-    public javax.ws.rs.core.Response createTrackInJSON(Juegos juego) {
-        Juegos res = db.createJuego(juego.getNombre(),juego.getEstado(),juego.getCategoria()
-                ,juego.getCosto(),juego.getVersion(),juego.getId_distribuidor(),juego.getDescripcion(),juego.getLinkImagen());
-        return javax.ws.rs.core.Response
-                .status(200)
-                .entity(res)
-                .build();
-    }
+
+
     @POST
     @Path("/postCompra")
     @Consumes(MediaType.APPLICATION_JSON)
@@ -34,4 +24,19 @@ public class JuegosWebApp {
                 .entity(res)
                 .build();
     }
+
+    /**
+     * metodo post para devolver juego
+     */
+
+    @POST
+    @Path("/post/devolucion/{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    //si no funciona usar javax.ws
+    public String deleteJuegofromUser(@PathParam("id") int id) {
+        String res = db.devolverJuego(id);
+        return res;
+    }
 }
+
